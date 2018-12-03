f74dc3953740425a2c1d13ad78e7c01afa82b025
AJackTi <dtrong97vn@gmail.com>
2018-12-03 11:13:02 +0700

    add entity framework code first for data

---
+++ TeduShop.Data/TeduShopDbContext.cs
@@ -0,0 +1,34 @@
+﻿using System.Data.Entity;
+using TeduShop.Model.Models;
+
+namespace TeduShop.Data
+{
+    public class TeduShopDbContext : DbContext
+    {
+        public TeduShopDbContext() : base("TeduShopConnection")
+        {
+            this.Configuration.LazyLoadingEnabled = false;
+        }
+        public DbSet<Footer> Footers{ get; set; }
+        public DbSet<Menu> Menus { get; set; }
+        public DbSet<MenuGroup> MenuGroups { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Page> Pages { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<PostCategory> PostCategories { get; set; }
+        public DbSet<PostTag> PostTags { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<ProductTag> ProductTags { get; set; }
+        public DbSet<Slide> Slides { get; set; }
+        public DbSet<SupportOnline> SupportOnlines { get; set; }
+        public DbSet<SystemConfig> SystemConfigs { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<VisitorStatistic> VisitorStatistics { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            
+        }
+    }
+}
