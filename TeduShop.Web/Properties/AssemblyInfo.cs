52088ddffaa1e3f8f6cc8b124078e3bf424bd706
AJackTi <dtrong97vn@gmail.com>
2018-12-05 17:38:37 +0700

    how to use web api

--- TeduShop.Web/Properties/AssemblyInfo.cs
+++ TeduShop.Web/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("50ad3428-41f1-41f2-8d7d-f9c9b138780c")]
+[assembly: Guid("ac99294f-3b88-41d4-9cb1-3edb0710f0a3")]
 
 // Version information for an assembly consists of the following four values:
 //
