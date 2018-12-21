271d97f896941ee4cabde5d16b7fd4fde4a14c2a
AJackTi <dtrong97vn@gmail.com>
2018-12-21 15:47:40 +0700

    add entity framework code first for data

---
+++ TeduShop.Data/Infrastructure/DbFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TeduShop.Data.Infrastructure
+{
+    internal class DbFactory : Disposable, IDbFactory
+    {
+        TeduShopDbContext dbContext;
+        public TeduShopDbContext Init()
+        {
+            return dbContext ?? (dbContext = new TeduShopDbContext());
+        }
+        protected override void DisposeCore()
+        {
+            if (dbContext != null)
+                dbContext.Dispose();
+        }
+    }
+}
---
+++ TeduShop.Data/TeduShopDbContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeduShop.Model.Models;
+
+namespace TeduShop.Data
+{
+    public class TeduShopDbContext : DbContext
+    {
+        public TeduShopDbContext():base("TeduShopConnection")
+        {
+            this.Configuration.LazyLoadingEnabled = false;
+        }
+        public DbSet<Footer> Footers { set; get; }
+        public DbSet<Menu> Menus { set; get; }
+        public DbSet<MenuGroup> MenuGroups { set; get; }
+        public DbSet<Order> Orders { set; get; }
+        public DbSet<OrderDetail> OrderDetails { set; get; }
+        public DbSet<Page> Pages { set; get; }
+        public DbSet<Post> Posts { set; get; }
+        public DbSet<PostCategory> PostCategories { set; get; }
+        public DbSet<PostTag> PostTags { set; get; }
+        public DbSet<Product> Products { set; get; }
+
+        public DbSet<ProductCategory> ProductCategories { set; get; }
+        public DbSet<ProductTag> ProductTags { set; get; }
+        public DbSet<Slide> Slides { set; get; }
+        public DbSet<SupportOnline> SupportOnlines { set; get; }
+        public DbSet<SystemConfig> SystemConfigs { set; get; }
+
+        public DbSet<Tag> Tags { set; get; }
+        // Chạy khi khởi tạo entity framework
+        protected override void OnModelCreating(DbModelBuilder builder)
+        {
+            //builder.Entity<IdentityUserRole>().HasKey(i => new { i.UserId, i.RoleId }).ToTable("ApplicationUserRoles");
+            //builder.Entity<IdentityUserLogin>().HasKey(i => i.UserId).ToTable("ApplicationUserLogins");
+            //builder.Entity<IdentityRole>().ToTable("ApplicationRoles");
+            //builder.Entity<IdentityUserClaim>().HasKey(i => i.UserId).ToTable("ApplicationUserClaims");
+        }
+    }
+}
