b8e6a5a928cee9b2392ed143157b475dc13ba91e
AJackTi <dtrong97vn@gmail.com>
2018-12-21 18:12:27 +0700

    add more repository

--- TeduShop.Data/Respositories/ProductCategoryRepository.cs
+++ TeduShop.Data/Respositories/ProductCategoryRepository.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TeduShop.Data.Infrastructure;
 using TeduShop.Model.Models;
 
-namespace TeduShop.Data.Respositories
+namespace TeduShop.Data.Repositories
 {
     public interface IProductCategoryRepository : IRepository<ProductCategory>
     {
